d0b3fa1b782f73b550674c6b4f5424657e524a0d
hell <fersilvavillamarin@gmail.com>
2021-08-27 17:13:17 -0300

    Atualizando hierarquia de pastas

--- app.py
+++ app.py
@@ -1,22 +1,15 @@
 from flask import Flask, render_template
 from pizza.pizza import pizzaBP
-import psycopg2
+
 
 
 app = Flask(__name__)
-
-
-POSTGRESQL_URI = "postgres://nrzaptwjbceonc:85e6f9cb1eb0447157fa9de8cc08cd804f02a1e555b5747860ec3a6d9f9140a0@ec2-35-153-91-18.compute-1.amazonaws.com:5432/d939kg82f0uljg"
-connection = psycopg2.connect(POSTGRESQL_URI)
-
-
 app.register_blueprint(pizzaBP)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 
 if __name__ == "__main__":
--- pizza/pizza.py
+++ pizza/pizza.py
@@ -1,27 +1,47 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+import flask
 import psycopg2
+import psycopg2.extras
 
 
 POSTGRESQL_URI = "postgres://nrzaptwjbceonc:85e6f9cb1eb0447157fa9de8cc08cd804f02a1e555b5747860ec3a6d9f9140a0@ec2-35-153-91-18.compute-1.amazonaws.com:5432/d939kg82f0uljg"
-connection = psycopg2.connect(POSTGRESQL_URI)
-
-
-
 pizzaBP = Blueprint('pizza', __name__, template_folder='templates', static_folder='static')
 
 
-@pizzaBP.route('/cardapio_pizza')
+@pizzaBP.route('/cardapio', methods=['GET'])
 def home_pizza():
-    return 'cardapio de pizza'
+    return render_template('cardapio.html')
 
 
-@pizzaBP.route('/cadastro_pizza')
+@pizzaBP.route('/cadastro_pizza', methods=['GET', 'POST'])
 def cadastro_pizza():
+    if flask.request.method == 'POST':
+        sabor = str(request.form['sabor']),
+        descricao = str(request.form['descricao']),
+        valor = float(request.form['valor'])
+        url_foto = str(request.form['url_foto'])
+        connection = psycopg2.connect(POSTGRESQL_URI)
+        with connection.cursor() as cursor:
+            sql = """insert into madarah.tb_pizza (sabor, descricao, valor, url_foto) VALUES (%s, %s, %s, %s)"""
+            cursor.execute(sql, (sabor, descricao, valor, url_foto))
+            cursor.close()
+            connection.commit()
     return render_template('cadastro.html')
 
 
-@pizzaBP.route('/edicao_pizza')
+
+@pizzaBP.route('/editar_pizza', methods=['GET', 'POST'])
 def edicao_pizza():
-    return 'edicao_pizza'
-
-
+    if flask.request.method == 'POST':
+        id = int(request.form['id']),
+        sabor = str(request.form['sabor']),
+        descricao = str(request.form['descricao']),
+        valor = float(request.form['valor'])
+        url_foto = str(request.form['url_foto'])
+        connection = psycopg2.connect(POSTGRESQL_URI)
+        with connection.cursor as cursor:
+            sql = """update madarah.tb_pizza SET sabor = (%s), descricao = (%s), valor = (%s), url_foto = (%s) WHERE id_pizza = (%s)"""
+            cursor.execute(sql, (sabor, descricao, valor, url_foto, id))
+            cursor.close()
+            connection.commit()
+    return render_template('edicao.html')
