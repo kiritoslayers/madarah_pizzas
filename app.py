68ba0f94084a01850700e3bb846565cfa1995113
Fernando <fersilvavillamarin@gmail.com>
2021-09-23 12:44:32 -0300

    rota registrar

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 from flask_login import LoginManager, login_manager, login_user
 from pizza.pizza import pizzaBP
 from login.login import loginBP
+from registar.registrar import registrarBP
 from usuario.usuario import userBP
 from carrinho.carrinho import carrinhoBP
 import psycopg2
@@ -22,7 +23,7 @@ app.register_blueprint(pizzaBP)
 app.register_blueprint(userBP)
 app.register_blueprint(loginBP)
 app.register_blueprint(carrinhoBP)
-
+app.register_blueprint(registrarBP)
 
 
 POSTGRESQL_URI = "postgres://nrzaptwjbceonc:85e6f9cb1eb0447157fa9de8cc08cd804f02a1e555b5747860ec3a6d9f9140a0@ec2-35-153-91-18.compute-1.amazonaws.com:5432/d939kg82f0uljg"
